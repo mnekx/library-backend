f21fa1b0f7972eaedb7bc0231ee992b705cbcff6
mnekx <karimnembac@gmail.com>
2022-09-30 00:09:01 +0300

    remove client folder

--- index.js
+++ index.js
@@ -113,8 +113,6 @@ app.post('/api/login', async (req, res) => {
   }
 });
 
-app.use(express.static(path.resolve(__dirname, '../client/build')));
-
 app.get('/api', (req, res) => {
   res.json({ message: 'Hello from server!' });
 });
